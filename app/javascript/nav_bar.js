81bdcce8ebcf6fb879d1e4f789fc179651e53cb9
Carolina Valladares <carolinalimacvalladares@gmail.com>
2024-05-27 20:35:09 -0300

    feat:add responsive nav

---
+++ app/javascript/nav_bar.js
@@ -0,0 +1,23 @@
+document.addEventListener("turbo:load", () => {
+  const nav = document.querySelector(".nav_links_container");
+  const closeBtn = document.querySelector(".nav_close_btn");
+  const overlay = document.querySelector(".overlay");
+  const hamburger = document.querySelector(".hamburger");
+
+  console.log("hi");
+
+  hamburger.addEventListener("click", () => {
+    nav.classList.add("open");
+    overlay.classList.add("open");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    nav.classList.remove("open");
+    overlay.classList.remove("open");
+  });
+
+  overlay.addEventListener("click", () => {
+    nav.classList.remove("open");
+    overlay.classList.remove("open");
+  });
+});
